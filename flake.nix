da717e690ffabcb7b8ca6f32338e8509ae506bcd
Antti Harju <antti@harju.io>
2026-04-11 16:05:04 +0300

    Help find-and-replace trigger container CI push

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Rust development environment";
+  description = "Rust development environment for rust-starter";
 
   nixConfig.extra-substituters = [
     "https://nix-community.cachix.org"
