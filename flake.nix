422ac4adc77b16d3d6aedf22ab8a58b69525ce3d
Antti Harju <antti@harju.io>
2026-04-13 21:44:11 +0300

    Sync with compare-changes

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
           rubocop
           shellcheck
           gh
+          zizmor
           # Everything below is required by GitHub Actions
           coreutils
           bash
@@ -143,7 +144,7 @@
         pkgs.lib.optionalAttrs (system == "x86_64-linux" || system == "aarch64-linux") {
           ci = pkgs.dockerTools.streamLayeredImage {
             name = "ci";
-            tag = "current";
+            tag = "flake";
             contents = (devPackages pkgs anttiharju system) ++ [
               ld
               zcc
